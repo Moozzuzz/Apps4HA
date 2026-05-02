3f4679d45a64d7723567b5952c0b30c05175bb7f
Moozzuzz <112196799+Moozzuzz@users.noreply.github.com>
2026-05-03 01:30:20 +0200

    Refactor miniDLNA entrypoint script

--- miniDLNA/run.sh
+++ miniDLNA/run.sh
@@ -1,38 +1,36 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bashio
 source /usr/lib/bashio/bashio.sh
 
-bashio::log.info "Start"
+bashio::log.info "Starten miniDLNA entrypoint"
 
-CONFIG_PATH=/data/options.json
-SYSTEM_USER=/data/system_user.json
-bashio::log.info "datapath loaded"
-
-declare ingress_interface
-declare ingress_port
-declare ingress_entry
-# curl -s -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" http://supervisor/core/info
-
-bashio::log.info "start ingress declaraties"
-ingress_port=$(bashio::addon.ingress_port)
-ingress_interface=$(bashio::addon.ip_address)
-ingress_entry=$(bashio::addon.ingress_entry)
-bashio::log.info "ingress declaratie klaar"
-
-MEDIA_DIR="$(bashio::config 'media_dir')"
-dirlist=$(echo $MEDIA_DIR | tr ";" "\n")
-friendly_name="$(bashio::config 'friendly_name')"
-
-sed -i "s/%%port%%/${ingress_port}/g" /etc/minidlna.conf
-
-for dir in $dirlist
-do
-    echo "> setting media dir: [media_dir=$dir]"
-	sed -i "/#media_dir/a \media_dir=$dir" /etc/minidlna.conf
+# Wacht op SUPERVISOR_TOKEN veilig
+TIMEOUT=30
+elapsed=0
+while bashio::var.is_empty "${SUPERVISOR_TOKEN:-}" && [ "$elapsed" -lt "$TIMEOUT" ]; do
+  sleep 1
+  elapsed=$((elapsed + 1))
 done
 
-sed -i "s/^#\(friendly_name\).*/\1=$friendly_name/" /etc/minidlna.conf
+if bashio::var.is_empty "${SUPERVISOR_TOKEN:-}"; then
+  bashio::log.warning "SUPERVISOR_TOKEN niet gevonden na ${TIMEOUT}s. Doorgaan zonder API-test."
+else
+  bashio::log.info "SUPERVISOR_TOKEN aanwezig, testen Supervisor API"
+  # Probeer unix socket eerst
+  if [ -S /var/run/supervisor.sock ]; then
+    if curl --silent --fail --unix-socket /var/run/supervisor.sock \
+      -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" http://localhost/supervisor/info >/dev/null 2>&1; then
+      bashio::log.info "Supervisor socket bereikbaar"
+    else
+      bashio::log.warning "Supervisor socket test mislukt"
+    fi
+  fi
 
-OPTIONS="$(bashio::config 'options')"
-bashio::log.info "Starting miniDLNA..."
+  if curl --silent --fail -H "Authorization: Bearer ${SUPERVISOR_TOKEN}" http://supervisor/core/info >/dev/null 2>&1; then
+    bashio::log.info "Supervisor HTTP endpoint bereikbaar"
+  else
+    bashio::log.warning "Supervisor HTTP test mislukt of token onvoldoende rechten"
+  fi
+fi
 
-exec /usr/sbin/minidlnad $OPTIONS
+bashio::log.info "Starten miniDLNA daemon"
+exec /usr/bin/minidlnad -f /etc/minidlna.conf -P /var/run/minidlna.pid
