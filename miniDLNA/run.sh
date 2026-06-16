7c26af091cb3cea0a6baf302b1c6beb1feb739ab
Moozzuzz <112196799+Moozzuzz@users.noreply.github.com>
2026-06-17 01:19:13 +0200

    Pass options to miniDLNA daemon on startup

--- miniDLNA/run.sh
+++ miniDLNA/run.sh
@@ -2,6 +2,7 @@
 source /usr/lib/bashio/bashio.sh
 
 bashio::log.warning "
+
 START
 "
 bashio::log.info "Starting miniDLNA entrypoint"
@@ -9,8 +10,7 @@ bashio::log.info "Starting miniDLNA entrypoint"
 bashio::log.warning "Setting variables"
 CONFIG_PATH=/data/options.json
 SYSTEM_USER=/data/system_user.json
-
-# find / -type f -name "*minidlnad*"
+OPTIONS="$(bashio::config 'options')"
 
 bashio::log.warning "Starten miniDLNA daemon"
-/usr/sbin/minidlnad -d -f /etc/minidlna.conf
+/usr/sbin/minidlnad -d -f /etc/minidlna.conf $OPTIONS
