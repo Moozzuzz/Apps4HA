79e3facb2193416f1ad7455471c235a8f90a0d0e
Moozzuzz <112196799+Moozzuzz@users.noreply.github.com>
2026-06-19 17:01:52 +0200

    Configure HTTP port in run.sh
    Added HTTP_PORT variable to configure the HTTP port dynamically.

--- miniDLNAretry/run.sh
+++ miniDLNAretry/run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/with-contenv bashio
+HTTP_PORT=$(bashio::config 'http_port')
 
-echo "Hello supervisor!"
+echo "Hello supervisor on port {$HTTP_PORT}!. API-token is:"
 echo $SUPERVISOR_TOKEN
 echo "Above is a token."
 
+exec /user/sbin/minidlna -f /etc/minidlna -P /run/minidlna.pid -d
