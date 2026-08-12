1a0d1bfd006bb8a385d41302d71ba4d5514f0d17
Moozzuzz <112196799+Moozzuzz@users.noreply.github.com>
2026-08-12 14:55:13 +0200

    Refactor run.sh to clean up commented code

--- miniDLNA/run.sh
+++ miniDLNA/run.sh
@@ -3,18 +3,9 @@ bashio::log.info "Start to run"
 
 CONFIG_PATH=/data/options.json
 SYSTEM_USER=/data/system_user.json
-VERSION=$(bashio::addon.version)
+VERSION="$(bashio::addon.version)"
 
-# declare ingress_port
-# declare ingress_interface
-# declare ingress_entry
-
-# ingress_port=$(bashio::addon.ingress_port)
-# ingress_interface=$(bashio::addon.ip_address)
-# ingress_entry=$(bashio::addon.ingress_entry)
-
-# echo "> setting ingress port "${ingress_port}
-# sed -i "s/%%port%%/${ingress_port}/g" /etc/minidlna.conf
+OPTIONS="$(bashio::config 'options')"
 
 MEDIA_DIR="$(bashio::config 'media_dir')"
 dirlist=$(echo $MEDIA_DIR | tr ";" "\n")
@@ -25,8 +16,6 @@ do
 	sed -i "/XXXmedia_dirXXX/a \media_dir=$dir" /etc/minidlna.conf
 done
 
-OPTIONS="$(bashio::config 'options')"
 bashio::log.info "MiniDLNA appversion $VERSION starting..."
 usr/sbin/minidlnad $OPTIONS
 bashio::log.info "MiniDLNA launched"
-
