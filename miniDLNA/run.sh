0142010b182a7de7c9d1008f380ce4d2596381da
Moozzuzz <112196799+Moozzuzz@users.noreply.github.com>
2026-08-12 14:15:04 +0200

    Update logging messages and comment out unused variables

--- miniDLNA/run.sh
+++ miniDLNA/run.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/with-contenv bashio
-bashio::log.info "Start"
+bashio::log.info "Start to run"
 
 CONFIG_PATH=/data/options.json
 SYSTEM_USER=/data/system_user.json
 
-declare ingress_interface
 declare ingress_port
-declare ingress_entry
+# declare ingress_interface
+# declare ingress_entry
 
 ingress_port=$(bashio::addon.ingress_port)
-ingress_interface=$(bashio::addon.ip_address)
-ingress_entry=$(bashio::addon.ingress_entry)
+# ingress_interface=$(bashio::addon.ip_address)
+# ingress_entry=$(bashio::addon.ingress_entry)
 
 MEDIA_DIR="$(bashio::config 'media_dir')"
 dirlist=$(echo $MEDIA_DIR | tr ";" "\n")
@@ -20,12 +20,12 @@ do
     echo "> setting media dir: [media_dir=$dir]"
 	sed -i "/XXXmedia_dirXXX/a \media_dir=$dir" /etc/minidlna.conf
 done
-#MEDIA_DIR2="$(bashio::config 'media_dir2')"
-#sed -i "s%XXXmedia_dir2XXX%$MEDIA_DIR2%g" /etc/minidlna.conf
 
 sed -i "s/%%port%%/${ingress_port}/g" /etc/minidlna.conf
-echo "> setting ingress port s/%%port%%/${ingress_port}/g"
+echo "> setting ingress port "${ingress_port}
 
 OPTIONS="$(bashio::config 'options')"
-bashio::log.info "Starting MiniDLNA..."
+bashio::log.info "MiniDLNA starting..."
 usr/sbin/minidlnad $OPTIONS
+bashio::log.info "MiniDLNA launched"
+
