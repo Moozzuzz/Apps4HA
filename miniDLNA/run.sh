e6777e3d3abe687c52b6503be51b047920ebfe01
Moozzuzz <112196799+Moozzuzz@users.noreply.github.com>
2026-08-12 14:46:02 +0200

    Clean up run.sh by removing unused ingress port code
    Removed commented lines related to ingress port settings and adjusted media directory handling.

--- miniDLNA/run.sh
+++ miniDLNA/run.sh
@@ -12,6 +12,9 @@ SYSTEM_USER=/data/system_user.json
 # ingress_interface=$(bashio::addon.ip_address)
 # ingress_entry=$(bashio::addon.ingress_entry)
 
+# echo "> setting ingress port "${ingress_port}
+# sed -i "s/%%port%%/${ingress_port}/g" /etc/minidlna.conf
+
 MEDIA_DIR="$(bashio::config 'media_dir')"
 dirlist=$(echo $MEDIA_DIR | tr ";" "\n")
 
@@ -20,9 +23,6 @@ do
     echo "> setting media dir: [media_dir=$dir]"
 	sed -i "/XXXmedia_dirXXX/a \media_dir=$dir" /etc/minidlna.conf
 done
-
-sed -i "s/%%port%%/${ingress_port}/g" /etc/minidlna.conf
-# echo "> setting ingress port "${ingress_port}
 
 OPTIONS="$(bashio::config 'options')"
 bashio::log.info "MiniDLNA starting..."
